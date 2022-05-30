7a35d29d1a78402dc400adc4fe37aa508fb5f944
ibrahim21010 <i.mohsen21010@gmail.com>
2022-05-30 16:47:14 +0200

    uplode Customers and Orders Files

--- resources/views/customers/customers.blade.php
+++ resources/views/customers/customers.blade.php
@@ -105,7 +105,7 @@
 
                             </tbody>
                         </table>
-                    </div>
+                    </div>g
                 </div>
             </div>
         </div>
